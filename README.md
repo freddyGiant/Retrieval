2fcdcabd08f48ba64dc88ce3aef5de48943089e7
vrad <vrad@tranticicero>
2025-04-10 10:24:15 -0400

    Trivial format changes
    These are opinionated, but help a lot with the readability and consistency (even the professionalism) of your code. Some lines only have whitespace changed.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Retrieval
-[USE LOCAL HOST] Final Project for 15-104. Interpretation of the idea of obscured memory and aging through images and particles. 
-My program starts with the image being clear then getting distorted when the particles age, representing the idea of aging throughout time and how memory gets forgotten as we age. To interact with the program, I allowed the user to ‘speed’ up the aging process and create distortion using mouseX and mouseY movement. My program relies on storytelling and letting the user watch as the images move through time. 
+
+[USE LOCAL HOST] Final Project for 15-104. Interpretation of the idea of obscured memory and aging through images and particles.
+My program starts with the image being clear then getting distorted when the particles age, representing the idea of aging throughout time and how memory gets forgotten as we age. To interact with the program, I allowed the user to ‘speed’ up the aging process and create distortion using mouse movement. My program relies on storytelling and letting the user watch as the images move through time.
--- particles.js
+++ particles.js
@@ -1,21 +1,18 @@
-
-
-function particleStep(){ 
-
-// controls aging
-    this.age+= 5;
+function particleStep() {
+    // controls aging
+    this.age += 5;
     this.x += this.dx;
     this.y += this.dy;
 
-    if (this.age > 255){
+    if (this.age > 255) {
         this.age = 255;
     }
 
-// controls the repeller of particles using mouseX & mouseY
+    // controls the repeller of particles using mouseX & mouseY
     var dp = dist(this.x, this.y, mouseX, mouseY);
     if (dp < 1) dp = 1;
 
-    var f = particleCount / (Math.pow(dp, 2)) *0.0025; 
+    var f = (particleCount / Math.pow(dp, 2)) * 0.0025;
     var dirx = (this.x - mouseX) / dp;
     var diry = (this.y - mouseY) / dp;
 
@@ -24,50 +21,51 @@ function particleStep(){
 
     if (this.x > width || this.x < 0) this.dx = -this.dx;
     if (this.y > height || this.y < 0) this.dy = -this.dy;
- 
 }
 
-function particleDraw(){
+function particleDraw() {
+    // draws the particles
 
-// draws the particles
-
-  fill(this.color.levels[0],this.color.levels[1],this.color.levels[2], 255 - this.age);
-  noStroke();
-  ellipse(this.x, this.y, random(1,5), random(1,5));
-
+    fill(
+        this.color.levels[0],
+        this.color.levels[1],
+        this.color.levels[2],
+        255 - this.age
+    );
+    noStroke();
+    ellipse(this.x, this.y, random(1, 5), random(1, 5));
 }
-function makeParticle(px, py, pdx, pdy, particleColor){ //particle object
-   return { 
+
+// particle object
+function makeParticle(px, py, pdx, pdy, particleColor) {
+    return {
         x: px,
         y: py,
-        dx:pdx,
+        dx: pdx,
         dy: pdy,
         age: 0,
         color: particleColor,
         stepfunction: particleStep,
-        drawFunction: particleDraw
+        drawFunction: particleDraw,
     };
 }
-function checkParticles(){ 
-    
-    //checks if the particles are done
 
+function checkParticles() {
+    //checks if the particles are done
     let allParticlesDone = true;
 
-    for(let i = 0; i < particles.length;i++){
-
+    for (let i = 0; i < particles.length; i++) {
         particles[i].stepfunction();
         particles[i].drawFunction();
 
-        if (particles[i].age < 255){
+        if (particles[i].age < 255) {
             allParticlesDone = false;
         }
     }
-    // controls when a new image appears 
-    if (allParticlesDone){
+
+    // controls when a new image appears
+    if (allParticlesDone) {
         IndexCounter = (IndexCounter + 1) % selfies.length;
         selfieSelection(IndexCounter);
     }
-
 }
-
